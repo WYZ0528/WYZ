95a4fe68c7a9286749bbca4467412a6825fee671
WYZ <55482401+WYZ0528@users.noreply.github.com>
2019-10-09 14:41:15 +0800

    Update fun1.c

--- project1/fun1.c
+++ project1/fun1.c
@@ -1,16 +1,36 @@
 #include"uhead.h"
 
-void show(int *array,int size){
-    for(int x=0;x<size;x++){
-        printf("%d    ",*(array+x));
-    }
+void show(int a[],int n)
+{
+	int i;
+	for (i=0;i<n;i++)
+  	{
+	 	printf("%d\n",a[i]);
+  	}
 }
 
-void uround(int *array,int size){
-    int num;
-    srand((unsigned)time(NULL));
-    for(int x=0;x<size;x++){
-        num=rand()%1000;
-        array[x]=num;
-    }
+void urand(int a[],int n)
+{
+	srand((int)time(0));
+	int i,j;
+	for (i=0;i<n;i++)
+  {
+		j=rand()%90+10;
+		a[i]=j;
+  }
+}
+
+char * itoa(int * a,int length){
+	char * c = (char *)malloc(sizeof(char)*(length*3+1));
+	int i,j;
+	int each;
+	for(i = 0,j = 1;i < length;i++,j+=3){
+		each = a[i] / 10;
+		c[j-1]=(char)(each + 48);
+		each = a[i] % 10;
+		c[j]=(char)(each + 48);
+		c[j+1]='\n';
+	}
+	c[length*3] = '\0';
+	return c;
 }
