c1cd78ae5f50b986e5690c248f6e668ccc4e23eb
WYZ <55482401+WYZ0528@users.noreply.github.com>
2019-10-09 14:44:07 +0800

    Update uhead.h

--- project1/uhead.h
+++ project1/uhead.h
@@ -1,22 +1,24 @@
-#include<stdlib.h>
 #include<stdio.h>
+#include<stdlib.h>
 #include<time.h>
-#include<math.h>
-#include<sys/stat.h>
-#include<sys/types.h>
-#include<unistd.h>
-#include<fcntl.h>
-struct result{//计算结果
-    long sum;
-    float average;
-};
+#include<string.h>
+#include <sys/time.h>
+#include <unistd.h>
 
-void show(int *,int);
-void uround(int *,int);//产生随机数,size为数量
-struct result uoprater(int *,int);//计算总数和平均数
-int comp(const void *a,const void *b);//比较大小
-void syssort(int *,int);//系统排序
-void usort(int *,int);//自己排序
-double calculate_time();//计算时间差
-void uwrite(const char *,int *,int);//写入文件
-void uread(const char *,int *,int);//读出文件
+struct arg
+{
+	float avg;
+	int sum;
+};
+void show(int*,int);
+void urand(int*,int);
+void readfile(char *path);
+void addfile(char  *append,char * path);
+int cmp(const void *a, const void *b);
+void SysSort(int a[],int n);
+void uSort(int a[], int n);
+int num_String(int x, char *s);
+char * itoa(int * a,int length);
+
+
+struct arg uoperator(int*,int);
